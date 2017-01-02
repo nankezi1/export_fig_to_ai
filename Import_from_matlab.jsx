dea4dcffd4824d777bb259c4d734bddb60eb042d
nschroet <schroeter_niels@hotmail.com>
2017-01-02 17:47:43 +0000

    added matlab gui and illustrator jsx
    currently, there is still a tiny offset between the axis in the
    generated .tif file and the borders of the vector axis in the .ai file

---
+++ Import_from_matlab.jsx
@@ -0,0 +1,18 @@
+﻿
+var docs = File.openDialog("Select Files to Open",true);  
+var docPdf=app.open(docs[0]); 
+var docTif=app.open(docs[1]); 
+
+docPdf.layers[0].pageItems[docPdf.layers[0].pageItems.length-1].remove(); //~clean the exported pdf of the last layer
+docPdf.layers[0].pageItems[docPdf.layers[0].pageItems.length-1].remove(); //~clean the exported pdf of the last layer
+
+var tif_copy=docTif.pageItems[0].duplicate(docPdf, ElementPlacement.PLACEATEND); //~copy tif to pdf and place at the bottom of the layer
+var tif_mask=docPdf.layers[0].pageItems[docPdf.layers[0].pageItems.length-2];
+
+var group =docPdf.groupItems.add();//~open new group that will become clipping group
+tif_copy.moveToBeginning(group); //~move last two items into group
+tif_mask.moveToBeginning(group); 
+group.clipped = true;
+group.zOrder( ZOrderMethod.SENDTOBACK );
+
+app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);  
---
+++ export_fig_to_ai.m
@@ -0,0 +1,150 @@
+function varargout = export_fig_to_ai(varargin)
+% EXPORT_FIG_TO_AI MATLAB code for export_fig_to_ai.fig
+%      EXPORT_FIG_TO_AI, by itself, creates a new EXPORT_FIG_TO_AI or raises the existing
+%      singleton*.
+%
+%      H = EXPORT_FIG_TO_AI returns the handle to a new EXPORT_FIG_TO_AI or the handle to
+%      the existing singleton*.
+%
+%      EXPORT_FIG_TO_AI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in EXPORT_FIG_TO_AI.M with the given input arguments.
+%
+%      EXPORT_FIG_TO_AI('Property','Value',...) creates a new EXPORT_FIG_TO_AI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before export_fig_to_ai_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to export_fig_to_ai_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help export_fig_to_ai
+
+% Last Modified by GUIDE v2.5 02-Jan-2017 17:44:16
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @export_fig_to_ai_OpeningFcn, ...
+                   'gui_OutputFcn',  @export_fig_to_ai_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before export_fig_to_ai is made visible.
+function export_fig_to_ai_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to export_fig_to_ai (see VARARGIN)
+
+% Choose default command line output for export_fig_to_ai
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes export_fig_to_ai wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = export_fig_to_ai_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function edit_filename_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_filename (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_filename as text
+%        str2double(get(hObject,'String')) returns contents of edit_filename as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_filename_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_filename (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+filename=get(handles.edit_filename,'String');
+axes_surfaces=findall(gca,'Type','Surface');
+axes_lines=findall(gca,'Type','Line');
+axes_contours=findall(gca,'Type','Contour');
+
+current_fig=gcf;
+
+
+
+
+% prepare figure for saving
+set(gca,'TickDir','out');
+xl = xlim;
+yl = ylim;
+zl = zlim;
+set(gca, 'FontName', 'Arial');
+
+set(current_fig,'Units','centimeters');
+pos = get(current_fig,'Position');
+set(current_fig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
+
+
+% set(gca,'TickLabelInterpreter', 'latex')
+
+set(gcf, 'Color', 'None')
+
+
+% save .tiff
+set(axes_lines,'Visible','off');
+set(axes_contours,'Visible','off');
+xlim(xl);
+ylim(yl);
+zlim(zl);
+
+print(current_fig,'-r600',filename,'-dtiffn');
+
+% save .eps
+set(gcf, 'Color', 'None')
+set(axes_lines,'Visible','on');
+set(axes_contours,'Visible','on');
+set(axes_surfaces,'Visible','off');
+% export_fig filename '-pdf' -transparent
+print(current_fig,'-r600',filename,'-dpdf');
+
+% switch all visibility back on
+set(axes_surfaces,'Visible','on');
+set(gcf, 'Color', 'white')
